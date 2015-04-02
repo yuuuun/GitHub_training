bac94c8eb6e2a63f37ab64d082a25b41b6dff29a
12YoU07 <yusuke@yusuke.local>
2015-04-02 19:07:20 +0900

    Modified : mysql_connect() function -> mysqli_connect()

--- db_test.php
+++ db_test.php
@@ -1,7 +1,8 @@
-<!--?php <br ?--> $link = mysql_connect('localhost', 'root' '');
+<!--?php
+$link = mysqli_connect('localhost', 'root' '');
 if (!$link){
-	die('接続失敗！'.mysql_error());
+	die('接続失敗！'.mysqli_connect_error());
 } else{
 	echo '接続成功！';
 }
-?$gt;
+?>
